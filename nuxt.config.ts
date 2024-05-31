e1e6cf6f239c9b57d1492e197f02ded21b3e317e
Deniz Zeybek <zeybekdeniz64@gmail.com>
2024-06-01 01:44:45 +0300

    add tailwind prose for md file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindTypography from "@tailwindcss/typography";
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [
-    "@nuxt/content",
-    '@nuxtjs/tailwindcss',
-    '@pinia/nuxt',
-  ],
-  css: ['~/assets/css/main.css'],
+  modules: ["@nuxt/content", "@nuxtjs/tailwindcss", "@pinia/nuxt"],
+  css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
+    },
+  },
+  tailwindcss: {
+    config: {
+      plugins: [tailwindTypography],
     },
   },
 });
--- pages/services/[slug].vue
+++ pages/services/[slug].vue
@@ -1,18 +1,20 @@
 <template>
   <main>
-    <header>
-        <h1>title: {{ data?.title }}</h1>
-        <h2 class="mb-5">description: {{ data?.description }}</h2>
-        <h5>author: {{ data?.author }}</h5>
+    <header class="">
+      <h1>title: {{ data?.title }}</h1>
+      <h2 class="mb-5">description: {{ data?.description }}</h2>
+      <h5>author: {{ data?.author }}</h5>
     </header>
-    <ContentRenderer :value="data!" />
+    <div class="prose prose-lg">
+      <ContentRenderer :value="data!" />
+    </div>
   </main>
 </template>
 
 <script setup lang="ts">
 const route = useRoute();
 const { data } = await useAsyncData(`content-${route.path}`, () =>
-  queryContent().where({ _path: route.path }).findOne()
+  queryContent().where({ _path: route.path }).findOne(),
 );
 </script>
 
