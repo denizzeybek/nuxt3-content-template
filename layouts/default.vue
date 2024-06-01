4ce6693f53fd9fed29b778fd5fd91e340d6eba02
Deniz Zeybek <zeybekdeniz64@gmail.com>
2024-06-01 03:31:48 +0300

    update default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,18 @@
 <template>
-  <template>
+  <div class="flex min-h-screen flex-col">
+    <Navbar />
     <main
       class="bg-r-white flex flex-1 flex-col rounded-lg p-2 lg:mb-5 lg:px-40"
     >
       <div id="alert" class="alert"></div>
-      <Navbar />
-      <section class="h-100vh flex flex-1 flex-col rounded-b-lg">
+      <section class="flex min-h-full flex-1 flex-col rounded-b-lg">
         <slot />
       </section>
-      <Footer />
     </main>
-  </template>
+    <Footer />
+  </div>
 </template>
+
 <script setup lang="ts">
 import Navbar from "~/components/local/layout/Navbar.vue";
 import Footer from "~/components/local/layout/Footer.vue";
