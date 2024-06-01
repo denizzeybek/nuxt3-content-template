edd4f73458dc12051b9abbe574ec7f92a669ebee
Deniz Zeybek <zeybekdeniz64@gmail.com>
2024-06-01 03:53:30 +0300

    update home page banner

--- components/local/home/Banner.vue
+++ components/local/home/Banner.vue
@@ -1,13 +1,24 @@
 <template>
-  <div class="grid grid-cols-1 justify-items-center lg:grid-cols-2 lg:justify-items-between gap-12 py-60">
-    <GlobalText as="span" className="text-6xl"
-      >Hello, I’m <br />
-      Mehmet Akif.
+  <div
+    class="lg:justify-items-between grid flex-1 grid-cols-1 justify-items-center gap-12 lg:grid-cols-2"
+  >
+    <!-- <div class=""> -->
+    <GlobalText as="span" className="text-6xl flex items-center"
+      >Merhaba, Ben <br />
+      Nazlı Kaçar.
     </GlobalText>
-    <GlobalText as="span" className="text-4xl text-center lg:text-end">
-      A senior-year design student who trying to specialize in 3D modeling &
-      texturing.
+    <GlobalText
+      as="span"
+      className="flex items-center text-2xl lg:text-2xl  text-center lg:text-end"
+    >
+      Nazlı Kaçar Hukuk ve Danışmanlık, Av.Nazlı Kaçar tarafından 2023 yılında
+      kurulmuştur. Türkçe, İngilizce ve Fransızca dillerinde hizmet verilen
+      büromuzda, 12’yi aşkın hukuk alanında, hem yerel hem yabancı müvekkillere
+      avukatlık ve danışmanlık hizmeti sunulmaktadır. Her türden dava süreci ve
+      uyuşmazlık hallerinde, yargılama sürecinin her aşamasında, müvekkil
+      öncelikli bir yaklaşım ile çözüm odaklı şekilde hizmet verilmektedir.
     </GlobalText>
+    <!-- </div> -->
   </div>
 </template>
 
